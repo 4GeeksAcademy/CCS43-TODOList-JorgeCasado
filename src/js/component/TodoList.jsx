a2b106e41fe5c9369a6f36094b69f675de20a934
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-07-12 17:44:23 +0000

    Counter ready

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,26 +1,20 @@
 import React, { useState } from "react";
 
-const TodoList = () => 
-{
+const TodoList = () => {
     const [task, setTask] = useState("");
 
     const [taskList, setTaskList] = useState([]);
 
     const [error, setError] = useState(false)
-    
-    const [active, setActive] = useState(false)
-    
-    const handleTask = (event) => 
-    {
-        setTask(event.target.value); 
-    } 
-    const addTask = (event) => 
-    {
-        if (event.key == "Enter") 
-        {
 
-            if (event.target.value == "") 
-            {
+    const [active, setActive] = useState(false)
+
+    const handleTask = (event) => {
+        setTask(event.target.value);
+    }
+    const addTask = (event) => {
+        if (event.key == "Enter") {
+            if (event.target.value == "") {
                 setError(true);
 
                 alert("Please add a task!");
@@ -34,36 +28,33 @@ const TodoList = () =>
             setError(false);
         }
     }
-    const deleteTask = (Id) => 
-    {
+    const deleteTask = (Id) => {
         console.log(Id);
 
         setTaskList(taskList.filter((task, index) => index != Id));
     }
-
     return (
 
         <div className="text-center vh-100 text-light bg-dark d-flex align-items-center justify-content-center gap-3 flex-column">
-            
+
             <label htmlFor="task" className={`${error ? "text-danger fw-bold" : "light"}`}><strong>Task List</strong></label>
-            
-            <input className="bg-dark text-light" type="text" id="task" value={task} onKeyDown={addTask} onChange={handleTask}/>
+
+            <input className="bg-dark text-light" type="text" id="task" value={task} onKeyDown={addTask} onChange={handleTask} />
             <div>
                 <ul>
-                {
-                    taskList.map((task, index) => 
                     {
-                        return <li className="task" key={index}>{task}<span className="delete" onClick={() => deleteTask(index)}> x </span></li>
-                    })
-                }
+                        taskList.map((task, index) => {
+                            return <li className="task" key={index}>{task}<span className="delete" onClick={() => deleteTask(index)}> x </span></li>
+                        })
+                    }
                 </ul>
+            </div>
+            <div>
+                {taskList.length} Items left
             </div>
         </div>
     );
 };
-
 export default TodoList;
 
-//onMouseOver={() => setActive(true)} onMouseOut={() => setActive(false)}
 
-//{active?"red":"mouseOver"}
